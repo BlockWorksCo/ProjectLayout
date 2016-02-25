7b8afd204ec49dfbf94ba78d4f365b336974fe7d
Steve Tickle <Steve@ZooKoo.com>
2016-02-25 07:12:01 +0000

    -

--- SourceLibrary/Interfaces/Safety/Safety.h
+++ SourceLibrary/Interfaces/Safety/Safety.h
@@ -13,6 +13,12 @@
 
 
 #include "Common.h"
+#include "ErrorHandling.h"
+
+
+#define PANIC_IF_ADDRESS_IS_NOT_READABLE( address )         if(IsPointerReadable() == false) PAINC()
+#define PANIC_IF_ADDRESS_IS_NOT_WRITABLE( address )         if(IsPointerWritable() == false) PAINC()
+#define PANIC_IF_ADDRESS_IS_NOT_EXECUTABLE( address )       if(IsPointerExecutable() == false) PAINC()
 
 
 bool IsPointerReadable( void* pointer );
