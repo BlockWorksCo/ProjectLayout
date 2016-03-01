bd14937e881aab03336aa0b0c5e443c2ea39f856
Steve Tickle <Steve@ZooKoo.com>
2016-03-01 22:10:10 +0000

    -

--- SourceLibrary/Implementations/ErrorHandling/Build.mk
+++ SourceLibrary/Implementations/ErrorHandling/Build.mk
@@ -9,7 +9,8 @@ VPATH		+=	$(SOURCE_LIBRARY)/Implementations/ErrorHandling
 # Include directories.
 #
 INCLUDES	+=	-I$(SOURCE_LIBRARY)/Implementations/ErrorHandling \
-        	  	-I$(SOURCE_LIBRARY)/Interfaces/ErrorHandling
+        	  	-I$(SOURCE_LIBRARY)/Interfaces/ErrorHandling \
+        	  	-I$(SOURCE_LIBRARY)/Interfaces/Common
 
 
 #
