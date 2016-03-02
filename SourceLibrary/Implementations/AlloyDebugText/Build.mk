b58ddbcacb00f86ac97417d6590c882765f75e37
Steve Tickle <Steve@ZooKoo.com>
2016-03-02 22:32:35 +0000

    -

---
+++ SourceLibrary/Implementations/AlloyDebugText/Build.mk
@@ -0,0 +1,24 @@
+
+#
+# Source directories
+#
+VPATH		+=	$(SOURCE_LIBRARY)/Implementations/AlloyDebugText
+
+
+#
+# Include directories.
+#
+INCLUDES	+=	-I$(SOURCE_LIBRARY)/Interfaces/DebugText
+
+
+#
+# Objects to build.
+#
+OBJS		+=	DebugText.o
+    		  		
+
+#
+# Libraries.
+#
+LIBS		+=
+
---
+++ SourceLibrary/Implementations/AlloyDebugText/DebugText.c
@@ -0,0 +1,53 @@
+//
+// Copyright (C) BlockWorks Consulting Ltd - All Rights Reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+// Written by Steve Tickle <Steve@BlockWorks.co>, September 2014.
+//
+
+
+
+#include "DebugText.h"
+#include "BoardSupport.h"
+#include "Alloy.h"
+
+
+
+
+
+//
+//
+//
+void DebugTextOutput(const char* text)
+{
+    uint32_t    coreID  = CoreNumber();
+
+    SystemCall  systemCall  = 
+    {
+        .type           = 0x00000001,
+        .payload        = (uint32_t)text,
+        .processedFlag  = false,
+    };
+
+    bridge->coreMessages[0][coreID].type    = 123;
+    bridge->coreMessages[0][coreID].payload = (uint32_t)&systemCall;
+    FlushCache();
+    TriggerMailboxInterrupt(0);            
+
+    while( systemCall.processedFlag == false );    
+}
+
+
+
+
+void DebugPrintf(const char* format, ...)
+{
+    static char     output[256];
+    
+    DebugTextOutput( &output[0] );
+}
+
+
+
+
+
