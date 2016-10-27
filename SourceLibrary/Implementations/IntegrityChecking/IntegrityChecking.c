9f10a7561d8cb586b3a2c78990b3e6efb358c1b1
Steve Tickle <Steve@ZooKoo.com>
2016-10-27 07:13:39 +0100

    -

---
+++ SourceLibrary/Implementations/IntegrityChecking/IntegrityChecking.c
@@ -0,0 +1,18 @@
+
+
+
+
+#include "IntegrityChecking.h"
+
+
+
+
+//
+//
+//
+uint32_t CRC32(uint32_t initialValue, uint8_t* data, uint32_t numberOfBytes)
+{
+
+}
+
+
---
+++ SourceLibrary/Interfaces/IntegrityChecking/IntegrityChecking.h
@@ -0,0 +1,21 @@
+
+
+
+
+#ifndef __INTEGRITYCHECKING_H__
+#define __INTEGRITYCHECKING_H__
+
+#include "Common.h"
+
+// crc32
+uint32_t CRC32(uint32_t initialValue, uint8_t* data, uint32_t numberOfBytes);
+
+//sha256
+
+// checksum
+
+// crc16
+
+#endif
+
+
