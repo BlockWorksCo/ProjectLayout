68a8be6ba78651a1d329c352f5efc9e3716e98c3
Steve Tickle <Steve@ZooKoo.com>
2016-10-28 23:18:32 +0100

    -

---
+++ SourceLibrary/Implementations/PersistentCircularBuffer/Tests/Build.mk
@@ -0,0 +1,24 @@
+
+#
+# Source directories
+#
+VPATH		+=	.
+
+
+#
+# Include directories.
+#
+INCLUDES	+=	-I.
+
+
+#
+# Objects to build.
+#
+OBJS		+=	Main.o
+    		  		
+
+#
+# Libraries.
+#
+LIBS		+=
+
