604fa1c23f7a00d152cb48272b6764d09face980
Steve Tickle <Steve@ZooKoo.com>
2016-10-29 19:09:01 +0100

    -

---
+++ SourceLibrary/Implementations/PersistentCircularBuffer/Tests/Output/Main.c
@@ -0,0 +1,71 @@
+
+
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "FLASHDevice.h"
+#include "PersistentCircularBuffer.h"
+#include "ErrorHandling.h"
+#include "Assertions.h"
+
+
+
+typedef struct
+{
+    bool        fieldA;
+    uint32_t    fieldB;
+    uint64_t    fieldC;
+
+} TestStruct;
+
+
+
+//
+//
+//
+void main()
+{
+    //
+    //
+    //
+    FLASHDeviceInitialise();
+
+    //
+    //
+    //
+    PersistentCircularBufferContext     pcbAContext;
+    PersistentCircularBufferLayout      pcbALayout      = LAYOUT( 0,16, sizeof(TestStruct) );
+    PersistentCircularBufferInitialise( &pcbAContext, &pcbALayout );
+
+    //
+    //
+    //
+    TestStruct testStructA  =
+    {
+        .fieldA     = true,
+        .fieldB     = 123,
+        .fieldC     = 321,
+    };
+    PersistentCircularBufferUpdateLast( &pcbAContext, (uint8_t*)&testStructA );
+
+    TestStruct testStructB  =
+    {
+        .fieldA     = false,
+        .fieldB     = 456,
+        .fieldC     = 654,
+    };
+    PersistentCircularBufferUpdateLast( &pcbAContext, (uint8_t*)&testStructB );
+
+    PersistentCircularBufferMoveToLast( &pcbAContext );
+    TestStruct  readBackA   = {0};
+    PersistentCircularBufferPeek( &pcbAContext, (uint8_t*)&readBackA );
+
+    AssertThat( readBackA.fieldA == false );
+    AssertThat( readBackA.fieldB == 456 );
+    AssertThat( readBackA.fieldB == 654 );
+
+    PersistentCircularBufferShowState( &pcbAContext );
+}
+
+
